8a21c02e4a58ed4fd31c90af53a0759700b52452
ishtourpedo <vishnuishu99@gmail.com>
2021-08-30 16:18:43 +0530

    springbootdemoproject

---
+++ src/test/java/com/example/Springbootdemoproject/SpringbootdemoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springbootdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
